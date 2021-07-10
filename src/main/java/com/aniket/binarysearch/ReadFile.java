3729f9b6da62ca25014f3d2d1ac10351ceae5404
Aniket-123-bit <anisunny782@gmail.com>
2021-07-10 13:09:15 +0530

    [Aniket]ADDS code for BinarySearch.

---
+++ src/main/java/com/aniket/binarysearch/ReadFile.java
@@ -0,0 +1,31 @@
+package com.aniket.binarysearch;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ReadFile {
+    public static ArrayList<String> readFile() {
+        ArrayList<String> dataArray = new ArrayList<>();
+        String data = " ";
+        String filePath = "C:\\Users\\ac\\IdeaProjects\\I\\BinarySearch\\src\\main\\java\\com\\aniket\\binarysearch\\data";
+        try {
+            File myObj = new File(filePath);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()){
+                data =myReader.nextLine().replaceAll(",", data);
+                String[] data1 = data.split("//W+");
+                for(String s: data1)
+                    dataArray.add(s);
+            }
+            myReader.close();
+
+        }catch (FileNotFoundException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return dataArray;
+
+    }
+}
